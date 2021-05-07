5977538ff0917b7c521b7bcc81ed4ac4c2756db1
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2021-05-07 15:15:51 +0900

    react로 만든 심플한 image gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, [term]); //'term'이 바뀔 때마다 이부분이 다시 실행될것임.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-6xl text-center mx-auto mt-32">
+          No images found..
+        </h1>
+      )}
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => {
+            return <ImageCard key={image.id} image={image}></ImageCard>;
+          })}
+        </div>
+      )}
     </div>
   );
 }
